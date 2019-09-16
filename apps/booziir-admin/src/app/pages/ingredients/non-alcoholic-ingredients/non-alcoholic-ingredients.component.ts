85059c2e8a4f6759fbb84db93d9db832029804f6
= <roman@muehlfeldner.net>
2019-09-16 19:15:13 +0200

    Added service to nonalcoholics container

--- apps/booziir-admin/src/app/pages/ingredients/non-alcoholic-ingredients/non-alcoholic-ingredients.component.ts
+++ apps/booziir-admin/src/app/pages/ingredients/non-alcoholic-ingredients/non-alcoholic-ingredients.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Ingredient } from '@booziir/shared';
+import { IngredientsNonAlcoholicService } from '../../../services/ingredients-non-alcoholic/ingredients-non-alcoholic.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'booziir-non-alcoholic-ingredients',
@@ -7,8 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NonAlcoholicIngredientsComponent implements OnInit {
 
-  constructor() { }
+  nonAlcoholics$: Observable<Ingredient[]>;
 
-  ngOnInit() {}
+  constructor(
+    private readonly nonAlcoholicsService: IngredientsNonAlcoholicService,
+    private readonly alertController: AlertController
+  ) {
+    this.nonAlcoholics$ = nonAlcoholicsService.items$;
+  }
 
+  ngOnInit() { }
+
+  async addNonAlcoholic(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Add Non Alcoholic',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Name'
+        },
+        {
+          name: 'available',
+          type: 'checkbox',
+          label: 'Available',
+          value: 'available',
+          checked: true
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'Add',
+          handler: (data) => {
+            console.log(data);
+          }
+        }
+      ]
+    })
+
+    await alert.present();
+  }
 }
