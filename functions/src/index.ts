997afd55c8e57e8f0b508e0035f7f290f01f6e3d
= <roman@muehlfeldner.net>
2019-09-22 10:18:27 +0200

    Added example cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,15 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+export const checkCocktailAvailability = functions
+    .region('europe-west1')
+    .firestore
+    .document('barUsers/{barUserId}/alcoholics/{ingredienId}')
+    .onUpdate((change, context) => {
+
+        console.log('INGREDIENT ID', change.after.id);
+
+        return true;
+    });
